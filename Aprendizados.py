a350550c3de2a624018af774e2f8da92f1e4ca00
Dev-Fernando-H <fhmp1@aluno.ifal.edu.br>
2024-10-08 10:52:27 -0300

    Add files via upload

---
+++ Aprendizados.py
@@ -0,0 +1,5 @@
+print ("Seja bem vindo ao QUIZZ do ENGLISH!")
+print ("Nesse jogo você tara que responder traduzindo para o Inglês algumas palavras em português, você iniciara esse jogo com 5 corações, se seus corações chegar a 0 você voltara do inicio!")
+coração = (5)
+p1 = input("Traduza EU SOU/EU ESTOU para o INGLÊS.") 
+if p1 : ("I AM")
