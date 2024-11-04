5b6971fb771a8892a148d9f0c434611c276e06f7
Dev-Fernando-H <fhmp1@aluno.ifal.edu.br>
2024-11-04 12:24:47 -0300

    Add files via upload

--- CalculadoraDeOperaçõesBasicas.py
+++ CalculadoraDeOperaçõesBasicas.py
@@ -1,7 +1,36 @@
 N1 = float(input("Digite um NUMERO!"))
 Op = input ("Digite uma OPERAÇÃO!")
 N2 = float(input("Digite outro NUMERO!"))
-if Op == "+": print (f"O resultado dessa soma é" , (N1) + (N2))
-elif Op == "-": print (f"O resultado dessa subtração é" , (N1) - (N2))
-elif Op == "*": print (f"O resultado dessa multiplicação é," , (N1) * (N2))
-elif Op == "/": print (f"O resultado dessa divisão é," , (N1) / (N2))
+Rs = ""
+if Op == "+":
+    Rs = N1 + N2
+    Rs = f"{Rs:_.2f}"
+    print (f"O resultado dessa soma é" , Rs . replace(".",",").replace("_","."))
+elif Op == "-":
+    Rs = N1 - N2
+    Rs = f"{Rs:_.2f}"
+    print (f"O resultado dessa subtração é" , Rs . replace(".",",").replace("_","."))
+elif Op == "*":
+    Rs = N1 * N2
+    Rs = f"{Rs:_.2f}"
+    print (f"O resultado dessa multiplicação é," , Rs . replace(".",",").replace("_","."))
+elif Op == "/":
+    
+    Rs = N1 / N2
+    
+    #definir a equação da função primeiro
+    
+    Rs = f"{Rs:_.2f}"
+    
+    #definir o "." como "_"
+    
+    print (f"O resultado dessa divisão é," , Rs . replace(".",",").replace("_","."))
+
+'''
+    mostrar o resultado trocando todos os pontos de Ex: 1.50 (um e meio em ingles) 
+    pelo formato brasileiro (1,50) e troca novamente o "_" por "." para ficar 1.500 ao em vez de 1_500
+'''
+
+
+
+
