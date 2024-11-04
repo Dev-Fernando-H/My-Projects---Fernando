0e147ee1f9181a25c362645288303329208179af
Dev-Fernando-H <fhmp1@aluno.ifal.edu.br>
2024-11-04 12:25:30 -0300

    Add files via upload

--- Aprendizados.py
+++ Aprendizados.py
@@ -1,5 +1,10 @@
-print ("Seja bem vindo ao QUIZZ do ENGLISH!")
-print ("Nesse jogo você tara que responder traduzindo para o Inglês algumas palavras em português, você iniciara esse jogo com 5 corações, se seus corações chegar a 0 você voltara do inicio!")
-coração = (5)
-p1 = input("Traduza EU SOU/EU ESTOU para o INGLÊS.") 
-if p1 : ("I AM")
+def main ():
+    print ("_ _ _ Calculadora de Quadrados _ _ _")
+    n = float(input("Qual o numero?"))
+    calcula_quadrado(n)
+
+    def calcula_quadrado(n):
+        print(f"O quadrado de {n} é {n * n}")
+
+
+        main()
